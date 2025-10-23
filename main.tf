1a1761134dce9246ecb01f201228d3680814099c
cedricxm2024 <cedricxm@hotmail.com>
2025-10-23 11:47:31 +0000

    #11 s3 bucket was maded and destroyed

--- main.tf
+++ main.tf
@@ -4,22 +4,33 @@ terraform {
       source = "hashicorp/random"
       version = "3.7.2"
     }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.17.0"
+    }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 
 resource "random_string" "random_bucket" {
-  length           = 16
+  lower = true
+  upper =  false
+  length           = 44
   special          = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.random_bucket.result
+
 }
 
 output "random_bucket_id" {
   value = random_string.random_bucket.id
-}
-
-output "random_bucket_result" {
-  value = random_string.random_bucket.result
 }
