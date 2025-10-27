71b2bddfb5b4b2be8033d67c6b373a112073421d
cedricxm2024 <cedricxm@hotmail.com>
2025-10-27 17:10:06 +0000

    #21 static website hosting setup is done

--- modules/video_game/outputs.tf
+++ modules/video_game/outputs.tf
@@ -7,3 +7,6 @@ output "gameing_bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.gameing_bucket.bucket
 }
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.site.website_endpoint
+}
