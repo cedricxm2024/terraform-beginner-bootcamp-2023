87d275dee1b607145b8d39173a52380e1a7967f0
cedricxm2024 <cedricxm@hotmail.com>
2025-10-29 11:41:29 +0000

    #27 terraform cloud is managing the state file !!
    Co-authored-by: Ona <no-reply@ona.com>

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-#resource "random_string" "random_bucket" {
-# lower = true
-#upper =  false
-#length           = 44
-#special          = false
-#}
-#
-#resource "aws_s3_bucket" "example" {
-# bucket = random_string.random_bucket.result
-#
-# tags = {
-#  UserUuid = var.user_uuid
-#}
-#}
+terraform { 
+  cloud { 
+    
+    organization = "example-org-e34b1a" 
+
+    workspaces { 
+      name = "terra-house-ent" 
+    } 
+  } 
+}
 
 module "video_game" {
   source    = "./modules/video_game"
